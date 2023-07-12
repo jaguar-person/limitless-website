3d8ab4738e4d4b9567906d456d7c11c93cc215d6
jaguar-person <catdogfriend51@gmail.com>
2023-07-12 15:51:19 +0200

    fix: _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import React from "react";
-import "../globals.css";
+import { NextPage } from "next";
+import type { AppProps } from "next/app";
 import localFont from "next/font/local";
+
+import "../globals.css";
+
+export type NextPageWithLayout<P = unknown, IP = P> = NextPage<P, IP> & {
+  hasNoLayout?: boolean;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+  pageProps: {
+    user?: unknown;
+  };
+};
 
 const poppins = localFont({
   src: [
@@ -16,7 +29,7 @@ const poppins = localFont({
   variable: "--font-poppins",
 });
 
-const MyApp = ({ Component, pageProps }) => {
+const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
   return (
     <main className={`${poppins.variable} font-sans`}>
       <Component {...pageProps} />
