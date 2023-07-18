e900abd586aebe1a415dd8da6c43fb4b1a14242c
jaguar-person <derekliman63@gmail.com>
2023-07-18 10:21:09 +0200

    fix: logo routing

--- src/layouts/TopBar.tsx
+++ src/layouts/TopBar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import LogoIcon from "../images/logo.svg";
 import NavItem from "../components/nav-item";
 import User from "../components/user";
@@ -8,6 +9,7 @@ import Menu from "../components/menu";
 import { isMobile } from "react-device-detect";
 
 const TopBar = () => {
+  const router = useRouter();
   return (
     <div className="flex justify-between items-center relative z-30">
       <Image
@@ -15,6 +17,9 @@ const TopBar = () => {
         width={isMobile ? 64 : 32}
         height={isMobile ? 64 : 32}
         alt="Logo"
+        onClick={() => {
+          router.push("/");
+        }}
       />
       <div className="hidden md:flex gap-10 text-lg text-secondary">
         <NavItem>Leaderboard</NavItem>
