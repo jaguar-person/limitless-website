0bcca1cbd9b4020a1cf3fc10735de5ea79087916
jaguar-person <catdogfriend51@gmail.com>
2023-07-19 18:13:20 +0200

    feat: leaderboard heatmap  page -mobile

--- src/modules/leaderboard/index.tsx
+++ src/modules/leaderboard/index.tsx
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import LeaderBoardSettings from "./settings";
 import LeaderBoardMain from "./main";
+import { useIsMobile } from "../../utils/hooks/useIsMobile";
+import Decoration from "../../components/decoration";
 
 const LeaderBoard: React.FC = () => {
   const [isLeaderBoard, setLeaderBoard] = useState(true);
+  const isMobile = useIsMobile();
   return (
-    <div className="mt-40 mb-20 gap-20 flex flex-col items-center">
-      <div className="text-white font-bold text-5xl">
-        Limitless Leaderboards
+    <div className="mt-16 md:mt-48 mb-48 md:mb-20 gap-20 flex flex-col items-center">
+      <div className="flex flex-col gap-4 text-white font-bold text-5xl items-center text-center">
+        <Decoration color="points" />
+        {!isLeaderBoard && isMobile ? "" : "Limitless Leaderboards"}
       </div>
       <div className="flex gap-3 w-full">
         <LeaderBoardSettings
--- src/modules/leaderboard/main/index.tsx
+++ src/modules/leaderboard/main/index.tsx
@@ -7,9 +7,9 @@ interface ILeaderboardMain {
 }
 
 const LeaderBoardMain: React.FC<ILeaderboardMain> = ({ flag }) => (
-  <div className="flex flex-col gap-2.5 flex-grow overflow-hidden">
+  <div className="flex flex-col gap-2.5 flex-grow overflow-hidden ">
     <div className="container">
-      <div className="flex justify-between">
+      <div className="hidden md:flex justify-between">
         <div className="flex gap-2.5 items-center">
           <div className="py-1 px-3.5 border border-points bg-points/[8%] text-points font-semibold text-xs">
             PVP
@@ -27,7 +27,13 @@ const LeaderBoardMain: React.FC<ILeaderboardMain> = ({ flag }) => (
           </div>
         </DropDown>
       </div>
-      {flag ? <LeaderBoardTable /> : <HeatMap />}
+      {flag ? (
+        <LeaderBoardTable />
+      ) : (
+        <div className="min-h-screen md:min-h-fit">
+          <HeatMap />
+        </div>
+      )}
     </div>
   </div>
 );
