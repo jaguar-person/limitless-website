eef22d12ab5e6cccf41a36b388e630ff6b1b695d
jaguar-person <derekliman63@gmail.com>
2023-07-20 11:46:02 +0200

    fix: leaderboard mobile

--- src/modules/leaderboard/index.tsx
+++ src/modules/leaderboard/index.tsx
@@ -8,7 +8,7 @@ const LeaderBoard: React.FC = () => {
   const [isLeaderBoard, setLeaderBoard] = useState(true);
   const isMobile = useIsMobile();
   return (
-    <div className="mt-16 md:mt-48 mb-48 md:mb-20 gap-20 flex flex-col items-center">
+    <div className="mt-16 md:mt-48 mb-48 md:mb-20 gap-16 md:gap-20 flex flex-col items-center">
       <div className="flex flex-col gap-4 text-white font-bold text-5xl items-center text-center">
         <Decoration color="points" />
         {!isLeaderBoard && isMobile ? "" : "Limitless Leaderboards"}
