ac95dee5d536056d10205d4cc3473ce86a8b6688
jaguar-person <catdogfriend51@gmail.com>
2023-07-25 12:03:39 +0200

    fix: This page gets cut off (doesn't fill the height)

--- src/modules/accounts/index.tsx
+++ src/modules/accounts/index.tsx
@@ -3,7 +3,7 @@ import TicketsUserInfo from "../tickets/user_info";
 import TicketsMain from "../tickets/main";
 
 const Accounts: React.FC = () => (
-  <div className="mt-6 md:mt-14 mb-52 md:mb-32 flex flex-col gap-7">
+  <div className="mt-6 md:mt-14 mb-52 md:mb-80 flex flex-col gap-7">
     <TicketsUserInfo />
     <TicketsMain />
   </div>
